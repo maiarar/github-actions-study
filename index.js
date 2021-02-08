7759655a0a21fdc3eb11744f96cb958750331ccf
Maiara Rodrigues <maiara.rodrigues@guiabolso.com>
2021-02-08 09:52:56 -0300

    adding actions file

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log(`connected to database ${process.env.DB_CONNECT}`)
     app.listen(3000, () => console.log('Server started and listening on port 3000!'));
-}); //.then((res) => {
-//     console.log(res)
-// });
+});
 
 app.set('view engine', 'ejs');
 
@@ -36,9 +34,7 @@ app.post('/', async(req, res) => {
         console.log('deu bom')
         res.redirect("/");
     } catch (err) {
-        console.log(`
-                    deu ruim\ n$ { err }
-                    `)
+        console.log(`deu ruim\n${err}`)
         res.redirect("/");
     }
 });
